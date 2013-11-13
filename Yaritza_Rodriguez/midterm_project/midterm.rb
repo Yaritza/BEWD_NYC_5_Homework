ba7f0b3aa7bd0ab714ed22b6049009c3643b8a3b
Yaritza <saturn540@gmail.com>
2013-11-13 17:21:16 -0500

    Cleaned up definitalize and accessor, Added return if no hits

--- Yaritza_Rodriguez/midterm_project/midterm.rb
+++ Yaritza_Rodriguez/midterm_project/midterm.rb
@@ -1,7 +1,7 @@
 
 
   class Posts
-        attr_reader :user_imput, :title, :author
+        attr_reader :user_imput
 
         def initialize user_imput
           @user_imput = user_imput
@@ -12,11 +12,11 @@
             keywords = @user_imput.split(' ').join('+').to_s #handles multi-word imput and number
 
            #want API to look like this: http://www.reddit.com/r/organic/search.json?q=_______&sort=relevance&restrict_sr=on&t=all')
-                                                                                                                          #^insert keyword(s) here
+                                                                                        #^subcategory               ^insert keyword(s) here
             api_begins ='http://www.reddit.com/r/organic/search.json?q='
             api_ends = '&sort=relevance&restrict_sr=on&t=all'
             api = api_begins.concat(keywords) << api_ends
-            #get api into quotes to proceed!
+
 
             search = RestClient.get(api)
             parsed = JSON.parse search
@@ -26,8 +26,11 @@
               puts "TITLE: #{post['data']['title']} \nPOSTED BY: #{post['data']['author']} \n ***"
             end
 
-          end
+              if results.empty?
+                puts "***Oh! Disculpa... There are no matches at this time.\nPlease try another search, or check us out at a diffent time. \nIf you'd like to further explore Organic Index, try searching for the word milk.***"
+            end
         end
+      end
 
 
       # result = RestClient.get('http://www.reddit.com/r/organic/search.json?q=keyword&sort=relevance&restrict_sr=on&t=all')
--- Yaritza_Rodriguez/midterm_project/runner.rb
+++ Yaritza_Rodriguez/midterm_project/runner.rb
@@ -2,11 +2,11 @@
     require 'json'
     require_relative 'midterm'
 
-puts "Hello and welcome to Organically Reddit! \nHere you can search for a listing of posts about news and the views of our \ncommunity in regards to anything relating to the concept of 'organic.
-\nWhat would you like to search?"
+puts "Hello and welcome to  Organic Index, powered by Reddit! \nHere you can search for an index of posts, including news as well as the views of the \nReddit community in regards to anything relating to the concept of *organic*.
+\nWhat would you like to search? (Kindly exclude symbols e.g: #, !,@, etc from your search)"
 
 user_imput = gets.chomp
-puts "Okay, here's a list of posts related to these term(s): #{user_imput}"
+puts "Okay, lets look for posts related to the term(s): #{user_imput}"
 
 listing = Posts.new(user_imput)
 
