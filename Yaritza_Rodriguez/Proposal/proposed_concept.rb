02009c5902e7cabaca6e1c29bbb436dd29228a7f
Yaritza <saturn540@gmail.com>
2013-11-18 15:24:23 -0500

    re-attempting to add proposal folders and contents

---
+++ Yaritza_Rodriguez/Proposal/proposed_concept.rb
@@ -0,0 +1,14 @@
+# Concept: USPS mailbox (post-office) locator app
+
+# To be made to fulfill the following criteria:
+
+# 1. Works off the USPS API to retrieve
+#      *mailbox addresses
+#      *mailbox pick-up times
+#      *May also include post-office locations and hours for further
+         user convinience
+# 2. Uses Google maps to search for those addresses using
+#      *a zip code
+#      *any address
+#      *or user's GPS location via your mobile device
+# 3. Mobile friendly for use on the go.
