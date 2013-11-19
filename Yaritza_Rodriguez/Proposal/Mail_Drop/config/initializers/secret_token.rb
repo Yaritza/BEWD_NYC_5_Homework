0635a287a1e5a6b8d32723099156caba77bcf4b6
Yaritza <saturn540@gmail.com>
2013-11-19 00:26:00 -0500

    initial (blank) bundle commit

---
+++ Yaritza_Rodriguez/Proposal/Mail_Drop/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MailDrop::Application.config.secret_key_base = '924423428096ee3fbaa16e50d951e6a675fa539eef2df30b48021c5bd41f034e3416d2b307ffab67b3e173fa47f43ed6ca93509f4d3dace7a04289fe1f579300'
---
+++ Yaritza_Rodriguez/Proposal/Mail_Drop/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MailDrop::Application.config.session_store :cookie_store, key: '_Mail_Drop_session'
