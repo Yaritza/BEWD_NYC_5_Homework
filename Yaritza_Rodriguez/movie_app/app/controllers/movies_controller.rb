564d3fa9e35c16814602788bdb06fad25c56074a
Yaritza <saturn540@gmail.com>
2013-12-07 16:24:20 -0500

    broken post validations

--- Yaritza_Rodriguez/movie_app/app/controllers/movies_controller.rb
+++ Yaritza_Rodriguez/movie_app/app/controllers/movies_controller.rb
@@ -12,15 +12,24 @@ class MoviesController < ApplicationController
     end
 
     def create
-       # raise params[:movie].inspect
+
+       raise params[:movie].inspect
+
         safe_movie = params.require(:movie).permit(:title, :description, :year_released, :rating)
-        @movies = Movie.create safe_movie
-        redirect_to @movies
+        @movie = Movie.create safe_movie
+
+        if @movie.save
+           flash[:notice] = "Successfully saved!"
+        redirect_to @movie
+
+      else
+        render 'new'
+      end
     end
 
-    def show
+      def show
        @movie = Movie.find params[:id]
-    end
+      end
 
     def edit
       @movie = Movie.find params[:id]
@@ -34,4 +43,9 @@ class MoviesController < ApplicationController
      redirect_to @movies
   end
 
+  private
+
+  def safe_movie_params
+      params.require(:movie).permit(:title, :description, :year_released, :rating)
+  end
 end
--- Yaritza_Rodriguez/movie_app/app/models/movie.rb
+++ Yaritza_Rodriguez/movie_app/app/models/movie.rb
@@ -1,5 +1,10 @@
 class Movie < ActiveRecord::Base
 
+validates :title, :description, presence: true #must have title & description
+validates :description, length: {minimum: 10} #description must be min. 10 chars
+validates :year_released, numericality: {greater_than: 0}
+validates :rating, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+
   def self.search_for (query)
     self.where('title LIKE :query OR description LIKE :query OR year_released LIKE :query',
                      query: "%#{query}%")
