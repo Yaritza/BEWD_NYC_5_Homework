81d51f9dbf355d01a53fef8cd30a759040413c21
Yaritza <saturn540@gmail.com>
2013-12-23 00:55:49 -0500

    fixed model

--- Yaritza_Rodriguez/Proposal/Mailbox_EnRoute/config/routes.rb
+++ Yaritza_Rodriguez/Proposal/Mailbox_EnRoute/config/routes.rb
@@ -1,4 +1,7 @@
 MailboxEnroute::Application.routes.draw do
+  get "/search", to: "search#index"
+  root 'mailboxes#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
